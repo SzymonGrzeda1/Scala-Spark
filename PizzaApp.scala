334fbcba240a18b6b625fa96dfd7a1ad84068494
SzymonGrzeda1 <szymon.marek.grzeda@gmail.com>
2025-03-29 19:06:26 +0100

    Avg pizza price

---
+++ PizzaApp.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.DataTypes
+
+object PizzaApp {
+  def main(args: Array[String]): Unit ={
+    val sparkSession: SparkSession = SparkSession.builder()
+      .appName("pizza-application" )
+      .master("local")
+      .getOrCreate()
+
+     import sparkSession.implicits._
+
+    val pizzaRawDF: Dataset[Row] = sparkSession.read
+      .option("header", "true")
+      .csv("pizza_data.csv")
+
+    val pizzaCleanDF: Dataset[Row] = pizzaRawDF.withColumn("Price", regexp_replace(col("Price"), "[$.]", "").cast(DataTypes.DoubleType))
+
+    val companiseWithAvgPricesDF: Dataset[Row] = pizzaCleanDF.groupBy("Company").avg("Price")
+
+    companiseWithAvgPricesDF.show()
+    companiseWithAvgPricesDF.printSchema()
+  }
+}
